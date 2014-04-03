7c6c62affc175b8c1589b4a534b80fa196d36ae0
Josh Matthews <josh@joshmatthews.net>
2014-04-02 23:41:30 -0400

    whatever

--- test.rs
+++ test.rs
@@ -5,6 +5,16 @@ pub mod foo {
   }
 }
 
+pub mod bar {
+    use super::foo::SomeStruct;
+    pub fn yay() {
+        let s = SomeStruct {
+            unused: 0,
+            to_rename: ~"whee"
+        };
+    }
+}
+
 fn main() {
   let s = foo::SomeStruct {
     unused: 0,
